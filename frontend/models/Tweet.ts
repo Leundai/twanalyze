55e6f2708d0f80c7378fad6da8cecd8cc322af60
Henry Fellerhoff <henryfellerhoff@gmail.com>
2020-10-08 13:35:42 -0500

    Tweak models and add dummy data

--- frontend/models/Tweet.ts
+++ frontend/models/Tweet.ts
@@ -14,7 +14,7 @@ export interface Tweet {
   text: string;
   likes: number;
   retweets: number;
-  time_created: Date; // Might be the incorrect type and may change - date is formatted "2020-10-07T17:09:37Z"
+  time_created: string; // Date is formatted "2020-10-07T17:09:37Z"
   sentiment: Sentiment;
   reply_sentiment?: Sentiment; // Sentiment of all replies; not implemented as of now
   replies: Tweet[];
---
+++ frontend/util/getDummyUserWithTweets.ts
@@ -0,0 +1,46 @@
+import { User } from '../models/Tweet';
+
+export const getDummyUserWithTweets = (): User => ({
+  name: 'Donald Trump',
+  username: 'realDonaldTrump',
+  profile_picture:
+    'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg',
+  tweets: [
+    {
+      text:
+        'The Great Commonwealth of Pennsylvania would absolutely die without the jobs and dollars brought in by Fracking. Massive numbers! Now Biden & Harris, after Radical Left Dem Primaries, are trying to change their stance. Pennsylvania wants Energy and Second Amendment!',
+      likes: 50300,
+      retweets: 13300,
+      time_created: '2020-10-08T11:11:37Z', // Might be the incorrect type and may change - date is formatted "2020-10-07T17:09:37Z"
+      sentiment: {
+        score: 7.1,
+        magnitude: 3.3,
+      },
+      replies: [],
+    },
+    {
+      text:
+        'The Great Commonwealth of Pennsylvania would absolutely die without the jobs and dollars brought in by Fracking. Massive numbers! Now Biden & Harris, after Radical Left Dem Primaries, are trying to change their stance. Pennsylvania wants Energy and Second Amendment!',
+      likes: 50300,
+      retweets: 13300,
+      time_created: '2020-10-08T11:11:37Z', // Might be the incorrect type and may change - date is formatted "2020-10-07T17:09:37Z"
+      sentiment: {
+        score: 7.1,
+        magnitude: 3.3,
+      },
+      replies: [],
+    },
+    {
+      text:
+        'The Great Commonwealth of Pennsylvania would absolutely die without the jobs and dollars brought in by Fracking. Massive numbers! Now Biden & Harris, after Radical Left Dem Primaries, are trying to change their stance. Pennsylvania wants Energy and Second Amendment!',
+      likes: 50300,
+      retweets: 13300,
+      time_created: '2020-10-08T11:11:37Z', // Might be the incorrect type and may change - date is formatted "2020-10-07T17:09:37Z"
+      sentiment: {
+        score: 7.1,
+        magnitude: 3.3,
+      },
+      replies: [],
+    },
+  ],
+});
