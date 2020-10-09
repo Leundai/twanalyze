8477463632d196f5803821098f3df7fc7085874e
Spencer McKee <srmckee@cs.washington.edu>
2020-10-09 16:34:15 -0700

    database integration with Flask and User data model

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -8,8 +8,36 @@ from mongoengine import *
 class User(Document, Mixin):
     """User Collection."""
 
-    userId = StringField(required=True)
+    class Tweet(EmbeddedDocument):
+
+        class Sentiment(EmbeddedDocument):
+            score = FloatField(required=True)
+            magnitude = FloatField(required=True)
+
+        class ReplySentiment(EmbeddedDocument):
+            score = FloatField(required=True)
+            magnitude = FloatField(required=True)
+
+        class Reply(EmbeddedDocument):
+            name = StringField(required=True)
+            username = StringField(required=True)
+            text = StringField(required=True)
+            likes = IntField(required=True)
+            retweets = IntField(required=True)
+            time_created = StringField(required=True)
+
+        text = StringField(required=True)
+        likes = IntField(required=True)
+        retweets = IntField(required=True)
+        time_created = StringField(required=True)
+        sentiment = EmbeddedDocumentField(Sentiment)
+        reply_sentiment = EmbeddedDocumentField(ReplySentiment)
+        replies = ListField(EmbeddedDocumentField(Reply))
+
+    name = StringField(required=True)
     username = StringField(required=True)
+    profile_picture = StringField(required=True)
+    tweets = ListField(EmbeddedDocumentField(Tweet))
 
     def __repr__(self):
-        return f"<User id:{self.userId} \n username:{self.username}>"
+        return f"<User id:{self.name} \n username:{self.username}>"
